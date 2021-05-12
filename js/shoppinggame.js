65c042d8be4b1fab8944fdcb2381e5afc819f24e
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-12 17:19:01 +0100

    test

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -36,16 +36,17 @@ const dateDiff = (date1, date2) => {
     return (date1.getTime() - date2.getTime()) * 1000 * 60 * 60 * 24;
  };
 
-
-
 // Here, use Object.defineProperty to create property - daysToExpire
 Object.defineProperty(Product.prototype, 'daysToExpire', {
     get: () => {
-        dateDiff(this.expiryDate, new Date());
+        return dateDiff(this.expiryDate, new Date());
     }
 });
 
 // Add method getDetails to Product here
+Product.prototype.getDetails = () => {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`;
+}
 
 // Define the MagicProduct class here
 
